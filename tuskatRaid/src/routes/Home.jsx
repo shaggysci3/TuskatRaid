fc34928f4303e7f9f417054e52caa285d8f8bb40
matthew <shaggysci3@gmail.com>
2024-06-28 13:39:30 -0400

    added album and fine tuned mobile hopefully better than before

--- tuskatRaid/src/routes/Home.jsx
+++ tuskatRaid/src/routes/Home.jsx
@@ -18,8 +18,18 @@ const Home = () =>{
             <Link className="start">Listen</Link>
             </div>
         </div>
-        <div className="albums">
-            <img src=""></img>
+        <div className="albumList">
+            <h1 className="albumName">featured Album: Terrapinix</h1>
+            <div className="albumContainer">
+            <img className="albumCover" src="https://cdn.pixilart.com/photos/large/e87ac512e08f0a1.png"></img>
+            <ul>
+                <h2>Songs</h2>
+                <li>The Terrapinix Terror</li>
+                <li>Crushing Defeat</li>
+                <li>Revegify</li>
+                <li>Tusk Town</li>
+            </ul>
+            </div>
         </div>
         {/* <h1>Home/Landing Page</h1> */}
         {/* <img src="https://cdn.pixilart.com/photos/large/0e27337e9a11f7d.png"></img> */}
