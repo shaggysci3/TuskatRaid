fb3dc6e24b7506b7993e40152523f9715a02cff2
matthew <shaggysci3@gmail.com>
2024-07-25 18:24:51 -0400

    database server is down

--- tuskatRaid/src/routes/Upcoming.jsx
+++ tuskatRaid/src/routes/Upcoming.jsx
@@ -12,18 +12,19 @@ const Upcoming = () =>{
 
  
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch("https://birds-ub6e.onrender.com/shows");
-      const ShowArr = await response.json();
-      setAllShows(ShowArr);
-    };
-    fetchProducts().catch(console.error);
-  }, []);
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const response = await fetch("https://birds-ub6e.onrender.com/shows");
+  //     const ShowArr = await response.json();
+  //     setAllShows(ShowArr);
+  //   };
+  //   fetchProducts().catch(console.error);
+  // }, []);
+  // console.log(allShows)
 
-  let shows = allShows.map((shows,index)=>{
-    return <Events key={index} id={shows.id} name={shows.name} date={shows.date} time={shows.time} img={shows.img} location={shows.location} />
-   })
+  // let shows = allShows.map((shows,index)=>{
+  //   return <Events key={index} id={shows.id} name={shows.name} date={shows.date} time={shows.time} img={shows.img} location={shows.location} />
+  //  })
   
 
     return(
@@ -37,8 +38,11 @@ const Upcoming = () =>{
           </>:<></>}
           {!allShows?<>
             <h1>shows loading please hold</h1>
-          </>:<>{shows}</>}
-         <Link style={{color:"black",borderColor:"black",marginTop:"20px"}} className="navLink"  to="/admin">login</Link>
+          </>:<>
+          <Events name={"Jam"} date={"10/10/2025"} time={"1030"} img={"https://cdn.pixilart.com/photos/large/e87ac512e08f0a1.png"} location={"boston"} />
+          {/* {shows} */}
+          </>}
+         {/* <Link style={{color:"black",borderColor:"black",marginTop:"20px"}} className="navLink"  to="/admin">login</Link> */}
 
 
       </div>
