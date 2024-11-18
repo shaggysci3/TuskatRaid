d849e979f71a1d2e27660090dc1f59caab086b7d
matthew <shaggysci3@gmail.com>
2024-11-17 22:01:40 -0500

    added styleing to contact and video player

--- tuskatRaid/src/routes/Contact.jsx
+++ tuskatRaid/src/routes/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () =>{
     return(
       <>
       <div className="pageContainer">
-        <div className="verticalContainer" style={{marginInline:"100px"}}>
+        <div className="verticalContainer" style={{marginInline:"100px", backgroundColor:"#F3EEE6",padding:"10px",textAlign:"center"}}>
         <h1>Email us at:</h1>
-        <h2 style={{fontFamily:"none"}}>Email: Brainsandwich@gmail.com</h2>
+        <h2 style={{fontFamily:"none"}}> Brainsandwich@gmail.com</h2>
         </div>
       </div>
         
