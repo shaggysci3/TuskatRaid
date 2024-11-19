11537a15e01414a445c22cb5d069747479ddd38e
matthew <shaggysci3@gmail.com>
2024-11-18 20:40:38 -0500

    adding new icons to band members on about page and ajusting mobile size/layout

--- tuskatRaid/src/routes/About.jsx
+++ tuskatRaid/src/routes/About.jsx
@@ -14,12 +14,16 @@ const About = () =>{
 
         <h1>about our band</h1>
         <p>Formed in a galaxy far, far away, Tusken and the raiders travel around playing new and local music for their legions of adoring fans. Comprising of:</p>
-        <ul>
-          <li>Brian</li>
-          <li>Justin</li>
-          <li>Alex</li>
-          <li>J.P.</li>
-        </ul>
+        <dl>
+          <dd>
+            <img style={{padding:"10px"}} className="bandIcon" src="https://cdn-icons-png.flaticon.com/512/4312/4312699.png"></img>Brian</dd>
+          <dd>
+          <img style={{padding:"10px"}} className="bandIcon" src="https://cdn-icons-png.flaticon.com/512/5096/5096217.png"></img>Justin</dd>
+          <dd>
+          <img style={{padding:"10px"}} className="bandIcon" src="https://cdn-icons-png.flaticon.com/512/26/26478.png"></img>Alex</dd>
+          <dd>
+          <img style={{padding:"10px"}} className="bandIcon" src="https://cdn-icons-png.flaticon.com/512/1789/1789712.png"></img>J.P.</dd>
+        </dl>
           <p>this fearsome foursome has played such venues as Mos Eisley Cantina, the Moons of Yavin Tavern, and Simerville Porchfest in Boston.</p>
       </div>
       </div>
